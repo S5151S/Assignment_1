9c2eff5be3df2994fa6b1ba8cbf24c357e1c4c2d
S5151S <snaidu.sambangi@gmail.com>
2018-08-25 09:03:41 +0530

    Create Assignment1.py

---
+++ Assignment1.py
@@ -0,0 +1,9 @@
+# .Below is a function in python that for each position in the list, sums up all numbers other than the number at that position. For example, given [1,2,3,4], it will return [9,8,7,6].Optimize this function so that it runs faster.
+    
+def special_sum():
+    lst=[1,2,3,4]
+    total=sum(lst)
+    total_li=[total-x for x in lst]
+    print total_li
+    
+special_sum()
